50a3e10a38a6696f73e7f4cf85a71f67622fcaac
Jefry Guevara <jefryeguevarav@gmail.com>
2024-09-05 20:23:13 -0500

    correcion blur fondo

--- src/components/view/layout/layout.tsx
+++ src/components/view/layout/layout.tsx
@@ -7,9 +7,9 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={style.bg}>
-      <div className={style.layout}>{children}</div>
-    </div>
+    // <div className={style.bg}>
+    <div className={style.layout}>{children}</div>
+    // </div>
   )
 }
 
